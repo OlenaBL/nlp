e827f5e6737aec88838fa52930195a9923b932e7
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2022-01-09 19:39:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,23 @@ def home():
 @app.route('/',methods=['POST'])
 def predict():
 	df = pd.read_csv('https://res.cloudinary.com/olena/raw/upload/v1621734607/csv/sentiment_2.csv')
-
+	stop = set(stopwords.words('english')) - set(['not', 'no', 'nor', "don't", 'very', 'down', 'most', 'over', 'such'])
+	#vectorizer = TfidfVectorizer(use_idf=True, lowercase=True, strip_accents='ascii', stop_words=stop)
 	from sklearn.feature_extraction.text import TfidfVectorizer#stop = set(stopwords.words('english')) - set(['not', 'no', 'nor', "don't", 'very', 'down', 'most', 'over', 'such'])
 	vectorizer = TfidfVectorizer(use_idf=True, lowercase=True, strip_accents='ascii')
 	y = df['label']
 	X = vectorizer.fit_transform(df['text'])
 	
 	from sklearn.model_selection import train_test_split
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 	
 	from sklearn.naive_bayes import MultinomialNB
-	clf = MultinomialNB()
+	clf = naive_bayes.MultinomialNB()
 	clf.fit(X_train, y_train)
-	clf.score(X_test,y_test)
+	#clf = MultinomialNB()
+	#clf.fit(X_train, y_train)
+	#clf.score(X_test,y_test)
 
 	if request.method == 'POST':
 		message = request.form['text']
